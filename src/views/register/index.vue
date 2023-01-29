1d1305e72a0fb1e2c4d50a403c70a2a2d9a859fc
t99999 <10258624+zhidu999@user.noreply.gitee.com>
2023-01-29 23:35:37 +0800

    项目_10_01_注册功能实习

--- src/views/register/index.vue
+++ src/views/register/index.vue
@@ -6,15 +6,15 @@
       <!-- 标题的盒子 -->
       <div class="title-box"></div>
       <!-- 注册的表单区域 -->
-      <el-form ref="form" :model="form" >
-        <el-form-item placeholder="请输入用户名">
-          <el-input v-model="form.username"></el-input>
+      <el-form ref="form" :model="form" :rules="rulesObj">
+        <el-form-item prop="username">
+          <el-input placeholder="请输入用户名" v-model="form.username"></el-input>
         </el-form-item>
-        <el-form-item placeholder="请输入密码">
-          <el-input v-model="form.password"></el-input>
+        <el-form-item prop="password">
+          <el-input type="password" placeholder="请输入密码" v-model="form.password"></el-input>
         </el-form-item>
-        <el-form-item placeholder="请再次输入密码">
-          <el-input v-model="form.repassword"></el-input>
+        <el-form-item prop="repassword">
+          <el-input type="password" placeholder="请再次输入密码" v-model="form.repassword"></el-input>
         </el-form-item>
         <el-form-item>
           <el-button class="btn-reg" type="primary" @click="registerFn">注册</el-button>
@@ -26,28 +26,77 @@
 </template>
 
 <script>
+
 // 经验：
 // 前端绑定数据对象属性名，可以直接给要调用的功能接口的参数名一致
 // 好处：我可以直接把前端对象（带着同名的属性和前端的值）发给后台
 export default {
   name: 'my-register',
   data () {
+    // 注意：必须在data函数里定义此箭头函数，才能确保this.form使用
+    const samePwdFn = (rule, value, callback) => {
+      if (value !== this.form.password) {
+        // 如果验证失败，则调用 回调函数时，指定一个 Error 对象
+        callback(new Error('两次输入的密码不一致!'))
+      } else {
+        // 如果验证成功，则直接调用callback 回调函数即可
+        callback()
+      }
+    }
+
     return {
       form: {
+        // 表单数据对象
         username: '', // 用户名
         password: '', // 密码
         repassword: '' // 确认密码
+      },
+      rulesObj: {
+        // 表单规则校验对象
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          {
+            pattern: /^[a-zA-Z0-9]{1,10}$/,
+            message: '用户名必须是1-10的大小写字母数字',
+            trigger: 'blur'
+          }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          {
+            pattern: /^\S{6,15}$/,
+            message: '密码必须是6-15的非空字符',
+            trigger: 'blur'
+          }
+        ],
+        repassword: [
+          { required: true, message: '请再次输入密码', trigger: 'blur' },
+          {
+            pattern: /^\S{6,15}$/,
+            message: '密码必须是6-15的非空字符',
+            trigger: 'blur'
+          },
+          { validator: samePwdFn, trigger: 'blur' }
+        ]
       }
     }
   },
   methods: {
     // 注册=》点击事件
     registerFn () {
-
+      //  js兜底校验
+      this.$refs.form.validate(valid => {
+        if (valid) {
+        // 通过校验,拿到绑定的数据
+          console.log(this.form)
+        } else {
+          return false // 阻止默认提交行为（表单下面红色提示会自动出现）
+        }
+      })
     }
   }
-
 }
+
 </script>
 
 <style lang="less" scoped>
