9e1e27e6eccbfc12d076256bce11d129b0df6247
t99999 <10258624+zhidu999@user.noreply.gitee.com>
2023-01-30 17:35:06 +0800

    项目_18保存用户信息到vuex

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,10 @@
 </template>
 
 <script>
-// import { getUserInfoAPI } from '@/api'
 export default {
-//   created () {
-//     getUserInfoAPI().then(res => {
-//       console.log(res)
-//     })
-//   }
+  created () {
+    this.$store.dispatch('getUserInfoActions')
+  }
 }
 </script>
 
