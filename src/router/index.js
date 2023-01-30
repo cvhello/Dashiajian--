7886ce5628f1192f4a0a85712904f68699f122bb
t99999 <10258624+zhidu999@user.noreply.gitee.com>
2023-01-30 18:56:11 +0800

    项目_19在路由守卫获取信息

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import store from '@/store'
 
 Vue.use(VueRouter)
 
@@ -25,4 +26,20 @@ const router = new VueRouter({
   routes
 })
 
+// 全局前置路由守卫
+router.beforeEach((to, from, next) => {
+  const token = store.state.token
+  if (token) {
+    // 你现在本地有token值， 才去请求用户的信息
+    store.dispatch('getUserInfoActions')
+  }
+  next()
+})
+
 export default router
+
+// 退出登录，重新登录，只走相关组件代码 (异步dom,切换不会导致所有代码重新执行，APP.vue不走)
+// 效果不对 ：换个账号他得重新请求用户数据呀
+// 解决:
+// 1.可以在登陆页面 登录成功后，再发送请求拿到用户信息
+// 2.可以在全局前置路由守卫中，写（路由跳转的时候 判断+获取）
