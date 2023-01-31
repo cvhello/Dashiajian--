61079de8f8e5df7a43e9f6bf04d52900bd4263e2
t99999 <10258624+zhidu999@user.noreply.gitee.com>
2023-01-31 18:45:53 +0800

    项目_25_权限拦截，未登录看不到页面

--- src/router/index.js
+++ src/router/index.js
@@ -26,14 +26,36 @@ const router = new VueRouter({
   routes
 })
 
+const whiteList = ['/login', '/reg']// 白名单
+// （无需登录可以访问的路由地址 ）
+
 // 全局前置路由守卫
+// 知识点1:浏览器第一次打开项目页面,会触发一次全局前置路由守卫函数
+// 知识点2:判断登录与否:有token就证明登陆了,没有token就wei
+// 知识点3:next()如果强制切换路由地址,,会不会走路由守卫
 router.beforeEach((to, from, next) => {
+  console.log(11)
+  // console.log(to, from)
+  // console.log(1)
   const token = store.state.token
-  if (token && !store.state.userInfo.username) {
-    // 你现在本地有token值， 才去请求用户的信息
-    store.dispatch('getUserInfoActions')
+  if (token) {
+    // 登陆了
+    if (!store.state.userInfo.username) {
+      // 你现在本地有token值， 才去请求用户的信息
+      store.dispatch('getUserInfoActions')
+    }
+    next()
+  } else {
+    // 未登录
+    // 数组。 includes(值)，作用：判断值是否在数组里面出现过，出现过原地返回true
+    if (whiteList.includes(to.path)) {
+      // 未登录，可以访问的路由地址，则放行(路由全局前置守卫不会再次触发了，而是匹配路由表，让组件挂载)
+      next()
+    } else {
+      // next() 强制切换到登录路径上
+      next('/login')
+    }
   }
-  next()
 })
 
 export default router
