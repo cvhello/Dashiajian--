2dfcf490a6579d9921cb2f09e27ffc73d7ca306f
t99999 <10258624+zhidu999@user.noreply.gitee.com>
2023-02-02 21:58:39 +0800

    项目_42文章分类

--- src/views/article/artCate.vue
+++ src/views/article/artCate.vue
@@ -3,7 +3,7 @@
     <el-card class="box-card">
       <div slot="header" class="clearfix header-box">
         <span>文章分类</span>
-        <el-button type="primary" size="mini" @click="addCateShowDialogFn"
+        <el-button type="primary" size="mini" @click=" addCateShowDialogBtnFn"
           >添加分类</el-button
         >
       </div>
@@ -50,8 +50,16 @@ vue2里它可以用多次，vue3把他移除了
 //运行时如下
 //
      -->
-    <el-dialog title="提示" :visible.sync="dialogVisible" width="30%">
-      <span>这是一段信息</span>
+    <el-dialog title="提示" :visible.sync="dialogVisible" width="30%" @close="dialogCloseFn">
+      <!-- 添加的表单 -->
+     <el-form :model="addForm" :rules="addRules" ref="addRef" label-width="80px">
+  <el-form-item label="分类名称" prop="cate_name">
+    <el-input v-model="addForm.cate_name" minlength="1" maxlength="10"></el-input>
+  </el-form-item>
+  <el-form-item label="分类别名" prop="cate_alias">
+    <el-input v-model="addForm.cate_alias" minlength="1" maxlength="15"></el-input>
+  </el-form-item>
+    </el-form>
       <span slot="footer" class="dialog-footer">
         <el-button @click="cancelFn">取 消</el-button>
         <el-button type="primary" @click="confirmFn">确 定</el-button>
@@ -67,7 +75,21 @@ export default {
   data () {
     return {
       cateList: [], // 文章分类数组
-      dialogVisible: false // 增加文章分类对话框--出现（true）隐藏（false）
+      dialogVisible: false, // 增加文章分类对话框--出现（true）隐藏（false）
+      addForm: { // 添加表单的数据对象
+        cate_name: '',
+        cate_alias: ''
+      },
+      addRules: { // 添加表单的验证规则对象
+        cate_name: [
+          { required: true, message: '请输入分类名称', trigger: 'blur' },
+          { pattern: /^\S{1,10}$/, message: '分类名必须是1-10位的非空字符', trigger: 'blur' }
+        ],
+        cate_alias: [
+          { required: true, message: '请输入分类别名', trigger: 'blur' },
+          { pattern: /^[a-zA-Z0-9]{1,15}$/, message: '分类别名必须是1-15位的字母数字', trigger: 'blur' }
+        ]
+      }
     }
   },
   created () {
@@ -81,7 +103,7 @@ export default {
       this.cateList = res.data.data
     },
     // 添加分类按钮点击事件=》为了对话框出现
-    addCateShowDialogFn () {
+    addCateShowDialogBtnFn () {
       this.dialogVisible = true
     },
     // 对话框确定按钮=》点击事件=》让对话框消失/调用保存文章分类接口
@@ -91,6 +113,10 @@ export default {
     // 对话框取消按钮=》点击事件
     cancelFn () {
       this.dialogVisible = false
+    },
+    // 对话框-关闭时的回调
+    dialogCloseFn () {
+      this.$refs.addRef.resetFields()
     }
   }
 }
