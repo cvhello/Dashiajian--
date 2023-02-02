85cae0a079878f208268d8499533200f2c25f9d5
t99999 <10258624+zhidu999@user.noreply.gitee.com>
2023-02-02 15:46:34 +0800

    项目34个人中心更换头像预览

--- src/views/user/userAvatar.vue
+++ src/views/user/userAvatar.vue
@@ -5,8 +5,8 @@
     </div>
     <div>
       <!-- 图片，用来展示用户选择的头像 -->
-      <img class="the_img" src="../../assets/images/avatar.jpg" alt="" />
-
+      <img v-if="!avatar" class="the_img" src="../../assets/images/avatar.jpg" alt="" />
+        <img v-else class="the_img" :src="avatar" alt="">
       <!-- 按钮区域 -->
       <div class="btn-box">
         <input type="file" accept="image/*" style="display: none" ref="iptRef" @change="onFileChange" />
@@ -22,7 +22,7 @@ export default {
   name: 'UserAvatar',
   data () {
     return {
-      avatar: ''
+      avatar: '' // 保存图片链接/base64字符串
     }
   },
   methods: {
@@ -43,6 +43,22 @@ export default {
       } else {
         // 证明看他选择了文件（默认只能选一个，但是如果选择多个需要给input标签加额外的原生熟悉）
         console.log(files[0])
+
+        // 目标：选择的图片文件，要给到img标签上做纯前端的预览
+        // img标签的src值，只能是图片链接地址（外链：http：//开头，图片文件相对路径
+        // 或者是图片的base64字符（而且字符串还必须data：image/png;base64,图片转base64字符串）
+        //  解决方案1：文件=》内存临时地址（这个地址只能是js里内存里不能发给后台）
+        // 语法：URL.careateObjectURL(文件)
+        // 返回值：内存临时地址
+        // this.avatar = URL.createObjectURL(files[0])
+
+        // 解决方案2：文件=》base64字符串（此字符串是可以发给后台的）
+        const fr = new FileReader()
+        fr.readAsDataURL(files[0]) // 传入文件对象开始读
+        fr.onload = (e) => { // onload等待把文件读成base64字符串后会触发onload事件函数
+        // e.target.result的值就是读完的结果
+          this.avatar = e.target.result // 赋予给变量，让他显示在img的src里
+        }
       }
     }
   }
