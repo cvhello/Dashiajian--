05f12960f2886d0ceeabbe0d48bc491eeef83c74
t99999 <10258624+zhidu999@user.noreply.gitee.com>
2023-02-02 20:57:55 +0800

    项目41文章分类添加分类对话框准备

--- src/views/article/artCate.vue
+++ src/views/article/artCate.vue
@@ -3,12 +3,18 @@
     <el-card class="box-card">
       <div slot="header" class="clearfix header-box">
         <span>文章分类</span>
-        <el-button type="primary" size="mini">添加分类</el-button>
+        <el-button type="primary" size="mini" @click="addCateShowDialogFn"
+          >添加分类</el-button
+        >
       </div>
       <!-- 分类数据表格 -->
       <el-table :data="cateList" style="width: 100%" border stripe>
         <!-- type是table-column内置属性，你告诉他用index，意思就是第一个单元格用索引值 -->
-        <el-table-column type="index" label="序号" width="100"></el-table-column>
+        <el-table-column
+          type="index"
+          label="序号"
+          width="100"
+        ></el-table-column>
         <el-table-column pro="cate_name"></el-table-column>
         <el-table-column pro="cate_alias"></el-table-column>
         <el-table-column label="操作">
@@ -17,6 +23,40 @@
         </el-table-column>
       </el-table>
     </el-card>
+
+    <!-- 添加文章分类的对话框
+    el-dialog 对话框组件
+    title 左上角标题
+    visible 控制对话框是否显示（右侧vue变量为true显示false隐藏）
+    visible.sync:组件内检测到对话框关闭（点击蒙层，按ESC，按右上角X）他会回传false给右侧变量
+before-close:对话框关闭前回调
+可以再内部用done()关闭弹窗
+什么都不调用就会阻止对话框关闭
+
+    扩展知识点：.sync是Vue2.3版本新增
+    官方文档https://v2.cn.vuejs.org/v2/guide/components-custom-events.html#sync-%E4%BF%AE%E9%A5%B0%E7%AC%A6
+    //.sync类似v-model是如何给子组件实现双向数据绑定(父Vue变量要给子组件，子组件传出的值也要自动绑定到父Vue变量
+    //)
+    //v-model本质:要给所在标签绑定：value="Vue变量" @input="val=>Vue变量=val"
+    <标签 v-model="Vue变量"><标签>
+运行如下
+<标签：value="Vue变量" @input="val=>Vue变量=val”><标签>
+Vue2里面一个标签上v-model只能使用一次，vue3里可以使用多次
+
+// .sync本质：给所在标签绑定：props属性名="vue变量" @update:props属性名="val =>Vue变量=val"
+//<标签 ：visible.sync="Vue变量" @update:visible="val=>vue变量名=val"></标签>
+//子组件内子传父的时候 this.$emit('update:visible')
+vue2里它可以用多次，vue3把他移除了
+//运行时如下
+//
+     -->
+    <el-dialog title="提示" :visible.sync="dialogVisible" width="30%">
+      <span>这是一段信息</span>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="cancelFn">取 消</el-button>
+        <el-button type="primary" @click="confirmFn">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -26,7 +66,8 @@ export default {
   name: 'ArtCate',
   data () {
     return {
-      cateList: [] // 文章分类数组
+      cateList: [], // 文章分类数组
+      dialogVisible: false // 增加文章分类对话框--出现（true）隐藏（false）
     }
   },
   created () {
@@ -38,6 +79,18 @@ export default {
       const res = await getArtCateListAPI()
       console.log(res)
       this.cateList = res.data.data
+    },
+    // 添加分类按钮点击事件=》为了对话框出现
+    addCateShowDialogFn () {
+      this.dialogVisible = true
+    },
+    // 对话框确定按钮=》点击事件=》让对话框消失/调用保存文章分类接口
+    confirmFn () {
+      this.dialogVisible = false
+    },
+    // 对话框取消按钮=》点击事件
+    cancelFn () {
+      this.dialogVisible = false
     }
   }
 }
