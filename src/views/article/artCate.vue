04f76741f128677ac34d4c0924ef7f01ce1f4e76
t99999 <10258624+zhidu999@user.noreply.gitee.com>
2023-02-04 16:55:40 +0800

    项目47文章分类删除的实现

--- src/views/article/artCate.vue
+++ src/views/article/artCate.vue
@@ -3,25 +3,32 @@
     <el-card class="box-card">
       <div slot="header" class="clearfix header-box">
         <span>文章分类</span>
-        <el-button type="primary" size="mini" @click=" addCateShowDialogBtnFn"
+        <el-button type="primary" size="mini" @click="addCateShowDialogBtnFn"
           >添加分类</el-button
         >
       </div>
       <!-- 分类数据表格 -->
-     <el-table style="width: 100%" :data="cateList" border stripe>
+      <el-table style="width: 100%" :data="cateList" border stripe>
         <!-- type是table-column内置属性，你告诉他用index，意思就是第一个单元格用索引值 -->
-      <el-table-column
+        <el-table-column
           label="序号"
           type="index"
           width="100"
         ></el-table-column>
-        <el-table-column prop="cate_name" label="分类名称" ></el-table-column>
+        <el-table-column prop="cate_name" label="分类名称"></el-table-column>
         <el-table-column prop="cate_alias" label="分类别名"></el-table-column>
         <el-table-column label="操作">
           <!-- scope对象：{row:行对象} -->
           <template v-slot="scope">
-            <el-button type="primary" size="mini" @click="updateCateBtnFn(scope.row)">修改</el-button>
-          <el-button type="danger" size="mini">删除</el-button>
+            <el-button
+              type="primary"
+              size="mini"
+              @click="updateCateBtnFn(scope.row)"
+              >修改</el-button
+            >
+            <el-button type="danger" size="mini" @click="delCateFn(scope.row)"
+              >删除</el-button
+            >
           </template>
         </el-table-column>
       </el-table>
@@ -53,16 +60,34 @@ vue2里它可以用多次，vue3把他移除了
 //运行时如下
 //
      -->
-    <el-dialog title="提示" :visible.sync="dialogVisible" width="30%" @close="dialogCloseFn">
+    <el-dialog
+      title="提示"
+      :visible.sync="dialogVisible"
+      width="30%"
+      @close="dialogCloseFn"
+    >
       <!-- 添加的表单 -->
-     <el-form :model="addForm" :rules="addRules" ref="addRef" label-width="80px">
-  <el-form-item label="分类名称" prop="cate_name">
-    <el-input v-model="addForm.cate_name" minlength="1" maxlength="10"></el-input>
-  </el-form-item>
-  <el-form-item label="分类别名" prop="cate_alias">
-    <el-input v-model="addForm.cate_alias" minlength="1" maxlength="15"></el-input>
-  </el-form-item>
-    </el-form>
+      <el-form
+        :model="addForm"
+        :rules="addRules"
+        ref="addRef"
+        label-width="80px"
+      >
+        <el-form-item label="分类名称" prop="cate_name">
+          <el-input
+            v-model="addForm.cate_name"
+            minlength="1"
+            maxlength="10"
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="分类别名" prop="cate_alias">
+          <el-input
+            v-model="addForm.cate_alias"
+            minlength="1"
+            maxlength="15"
+          ></el-input>
+        </el-form-item>
+      </el-form>
       <span slot="footer" class="dialog-footer">
         <el-button @click="cancelFn">取 消</el-button>
         <el-button type="primary" @click="confirmFn">确 定</el-button>
@@ -77,7 +102,12 @@ vue2里它可以用多次，vue3把他移除了
 // 2.在点击修改的时候，isEdit改为true,editId保存要修改的数据id
 // 3.在点击新增按钮的时候，isEdit改为false editId置空
 // 4.在点击保存按钮时(确定按钮时，就可以用isEdit变量做区分了、)
-import { getArtCateListAPI, saveArtCateAPI, updateArtCateAPI } from '@/api'
+import {
+  getArtCateListAPI,
+  saveArtCateAPI,
+  updateArtCateAPI,
+  delArtCateAPI
+} from '@/api'
 
 export default {
   name: 'ArtCate',
@@ -85,22 +115,32 @@ export default {
     return {
       cateList: [], // 文章分类数组
       dialogVisible: false, // 增加文章分类对话框--出现（true）隐藏（false）
-      addForm: { // 添加表单的数据对象
+      addForm: {
+        // 添加表单的数据对象
         cate_name: '',
         cate_alias: ''
       },
-      addRules: { // 添加表单的验证规则对象
+      addRules: {
+        // 添加表单的验证规则对象
         cate_name: [
           { required: true, message: '请输入分类名称', trigger: 'blur' },
-          { pattern: /^\S{1,10}$/, message: '分类名必须是1-10位的非空字符', trigger: 'blur' }
+          {
+            pattern: /^\S{1,10}$/,
+            message: '分类名必须是1-10位的非空字符',
+            trigger: 'blur'
+          }
         ],
         cate_alias: [
           { required: true, message: '请输入分类别名', trigger: 'blur' },
-          { pattern: /^[a-zA-Z0-9]{1,15}$/, message: '分类别名必须是1-15位的字母数字', trigger: 'blur' }
+          {
+            pattern: /^[a-zA-Z0-9]{1,15}$/,
+            message: '分类别名必须是1-15位的字母数字',
+            trigger: 'blur'
+          }
         ]
       },
       isEdit: false, // 默认新增 true为编辑状态，false为新增状态
-      editId: ''// 保存正在要编辑的数据id值
+      editId: '' // 保存正在要编辑的数据id值
     }
   },
   created () {
@@ -121,14 +161,17 @@ export default {
     },
     // 对话框确定按钮=》点击事件=》让对话框消失/调用保存文章分类接口
     confirmFn () {
-      this.$refs.addRef.validate(async valid => {
+      this.$refs.addRef.validate(async (valid) => {
         if (valid) {
           // 通过校验
           if (this.isEdit) {
             // 要修改
             // this.addForm.id = this.editId // 把要编辑的文章分类id添加到对象身上
             //  updateArtCateAPI(this.addForm)
-            const { data: res } = await updateArtCateAPI({ id: this.editId, ...this.addForm })
+            const { data: res } = await updateArtCateAPI({
+              id: this.editId,
+              ...this.addForm
+            })
             if (res.code !== 0) return this.$message.error(res.message)
             this.$message.success(res.message)
           } else {
@@ -172,6 +215,16 @@ export default {
         this.addForm.cate_name = obj.cate_name
         this.addForm.cate_alias = obj.cate_alias
       })
+    },
+    // 删除分类按钮=》点击事件
+    async delCateFn (obj) {
+      // obj:{id,cate_name，cate_alias}
+      const { data: res } = await delArtCateAPI(obj.id)
+      if (res.code !== 0) return this.$message.error(res.message)
+      this.$message.success(res.message)
+
+      // 删除后，再调用方法去后台在获取最新的数据列表
+      this.getArtCateFn()
     }
   }
 }
