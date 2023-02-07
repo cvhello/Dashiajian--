3a2baf1add82c98e7fa0bc3bad207c5b67218368
t99999 <10258624+zhidu999@user.noreply.gitee.com>
2023-02-07 19:33:10 +0800

    项目_82_打包注意的两个点和环境的区分

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
+/* eslint-disable no-const-assign */
 const { defineConfig } = require('@vue/cli-service')
 // 需要排除的包对象
 let externals = {}
+// 需要配置的CDN 链接
+let CDN = { css: [], js: [] }
 // 判断是否是生产环境
 const isProduction = process.env.NODE_ENV === 'production'
 // 如何是生产环境，需要执行以下逻辑
@@ -25,13 +28,34 @@ if (isProduction) {
     'vue-quill-editor': 'VueQuillEditor',
     'vuex-persistedstate': 'createPersistedState'
   }
+  CDN = {
+    css: [ // element-ui css样式表
+      'https://unpkg.com/element-ui@2.15.8/lib/theme-chalk/index.css',
+      'https://unpkg.com/quill@1.3.7/dist/quill.core.css',
+      'https://unpkg.com/quill@1.3.7/dist/quill.snow.css',
+      'https://unpkg.com/quill@1.3.7/dist/quill.bubble.css'
+    ],
+
+    js: [
+      'https://unpkg.com/echarts@5.3.2/dist/echarts.min.js',
+      'https://unpkg.com/vue@2.6.14/dist/vue.js',
+      'https://unpkg.com/vue-router@3.5.1/dist/vue-router.js',
+      'https://unpkg.com/vuex@3.6.2/dist/vuex.js',
+      'https://unpkg.com/axios@0.27.2/dist/axios.min.js',
+      'https://unpkg.com/dayjs@1.11.3/dayjs.min.js',
+      'https://unpkg.com/element-ui@2.15.8/lib/index.js',
+      'https://unpkg.com/quill@1.3.7/dist/quill.js',
+      'https://unpkg.com/vue-quill-editor@3.0.6/dist/vue-quill-editor.js',
+      'https://unpkg.com/vuex-persistedstate@3.2.1/dist/vuex-persistedstate.umd.js'
+    ]
+  }
 }
 module.exports = defineConfig({
   transpileDependencies: true,
   // 影响打包时，index.html引入其他资源的前缀地址
   // ./可以让开发环境和生成环境都可以正常使用
   // 开发环境：'/'
-  // 生成环境：'./'
+  // 生产环境：'./'
   //  问题：有无代码可以让他自己识别当前运行环境？
   // node里有个内置的环境变量process.env.NODE_ENV
   // process.env.NODE_ENV他会根据你敲击的命令，使用不同的值
@@ -49,6 +73,14 @@ module.exports = defineConfig({
     //     '@': resolve('src')
     //   }
     // }
+  },
+  chainWebpack (config) {
+    // 注入cdn变量（打包时会执行）
+    config.plugin('html').tap(args => {
+      // 参数对象添加属性叫cdn，值就是上面CDN对象
+      args[0].cdn = CDN // 配置CDN给插件
+      return args
+    })
   }
 })
 
@@ -58,3 +90,16 @@ module.exports = defineConfig({
 // 开发环境，pub;oc/index.html,也有那些第三方的cdn地址，所有也能正常运行
 // 但是第一次速度有点慢，所以开发环境呢我还是想让webpack引入本地的node_modules那些第三方包
 // process.env.NODE_ENV 可以根据敲击的命令不同，值也不同，能够一个代码区分不同环境
+
+// 总结：
+// 1.publicPath:影响的是打包(webpack开发服务器/dist)的时候index.html引入其他资源的前缀地址
+// 情况1:开发环境，值：'/'
+// 情况2：生产环境，值：'./' (才能确保dist/index.html相对路径下访问)
+
+// 2.dist瘦身：影响dist的体积，其他第三方包用cdn地址引入到index.html中保存运行
+// 情况1：
+// (1):开发环境，externals无值(不排除第三方包)
+// (2):index.html里也不引入cdn地址
+// 情况2：生产环境
+// (1):externals有值(排除第三方包)
+// (2):index.html引入cdn地址
