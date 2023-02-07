0d7af38d6ade5ad21b80eefa9e6dc3f0195535bd
t99999 <10258624+zhidu999@user.noreply.gitee.com>
2023-02-07 13:39:05 +0800

    项目_76打包发布_publicPatg的值修改

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: './'
+  // 影响打包时，index.html引入其他资源的前缀地址
+  // ./可以让开发环境和生成环境都可以正常使用
+  // 开发环境：'/'
+  // 生成环境：'./'
+  //  问题：有无代码可以让他自己识别当前运行环境？
+  // node里有个内置的环境变量process.env.NODE_ENV
+  // process.env.NODE_ENV他会根据你敲击的命令，使用不同的值
+  // 解决：
+  // 如果你敲击yarn build ，process.env.NODE_ENV的值就是 'production'字符串
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/'
+  // publicPath: './'
 })
